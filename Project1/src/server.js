13df0d8607f0078522544cf7fe170dbb6162616c
Francisco <franciscoabreu1408@gmail.com>
2023-08-10 09:58:53 -0300

    Criando req em JSON

--- Project1/src/server.js
+++ Project1/src/server.js
@@ -11,20 +11,21 @@ const server = http.createServer(async (req, res) =>{
     for await (const chunk of req) {
         buffers.push(chunk)
     }
-
+// ------ Transforma o corpo da requisicao feita em JSON ------
     try {
         req.body = JSON.parse(Buffer.concat(buffers).toString())
     } catch {
         req.body = null
     }
 
-
+//-------- Resposta é users ja cadastrados em formato de string -----
     if (method === 'GET' && url === '/users') {
         return res
             .setHeader('Content-type', 'application/json')
             .end(JSON.stringify(users))
     }
 
+//------ Adiciona na lista de usuarios o corpo da req -----
     if (method === 'POST' && url === '/users') {
         const {name, email} = req.body
 
